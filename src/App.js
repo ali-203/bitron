a4c004bfb572057aba708a00f2e8bc7b9f8a9771
ali-203 <62180010+ali-203@users.noreply.github.com>
2021-02-01 23:45:32 +0500

    updated css

--- src/App.js
+++ src/App.js
@@ -1,54 +1,37 @@
 import React from 'react';
 import "./App.css";
-
-
+import Image from "./images/image.png";
 
 function App() {
     return (
-        
         <div className="main">
-        
-        
-        
-       <div className="bitron_main">
-      <div className="smart"> <p>SMART</p>
-       <div className="vrtl">line</div>
-       </div>
-       
-      <div className="bi"> <h1>BI</h1></div>
-      <div className="tron"> <h1>TRON</h1></div>
-      
-
-      <div className="vrtl2">
-      <div>
-      <p>CONTRACT</p>
-      </div>
-      </div>
-
-       
-       
-
-
-       <div className="decentrlized">
-           <h1 className="best">BEST DECENTRALIZED</h1>
-           <h1 className="community">COMMUNITY SUPPORT FUND ON TRON BLOCKCHAIN</h1>
-           <h2 className="verified">100% VERIFIED SMART CONTRACT</h2>
-           <h3 className="daily">18.28% Daily ROI
-           128% IN 7 DAYS</h3>
-       
-       
-       <img src="image.png" className="images"></img>
-       </div>
-       <div className="button">
-           <button>TELEGRAM</button>
-           <button>SMART CONTRACT</button>
-       </div>
-       </div>
-       
-       </div>
-       
-       
-       
+            <div className="bitron_main">
+                <div className="smart"> <p>SMART</p>
+                    <div className="vrtl">line</div>
+                
+                <div className="bi"> <h1>BI</h1></div>
+                <div className="tron"> <h1>TRON</h1></div>
+                <div className="vrtl2">
+                    <div>
+                        <p>CONTRACT</p>
+                    </div>
+                    </div>
+                </div>
+                <div className="decentrlized">
+                    <h1 className="best">BEST DECENTRALIZED</h1>
+                    <h1 className="community">COMMUNITY SUPPORT FUND ON TRON BLOCKCHAIN</h1>
+                    <h2 className="verified">100% VERIFIED SMART CONTRACT</h2>
+                    <h3 className="daily">18.28% Daily ROI128% IN 7 DAYS</h3>
+                </div>
+                <div className="button">
+                    <button>TELEGRAM</button>
+                    <button>SMART CONTRACT</button>
+                </div>
+            </div>
+            <div className="image">
+                <img src={Image} />
+            </div>
+        </div>
     )
 }
 
